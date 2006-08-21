73d9049cf3b131db9b7f31775722c82b74ff603d
jdscheff <jdscheff@86c54e43-84fa-0310-a037-bc9176f9ec66>
2006-08-21 00:41:18 +0000

    warning before database backup

--- sources/admin/backup_database.php
+++ sources/admin/backup_database.php
@@ -22,31 +22,43 @@ if (!defined('ATSPHP')) {
 
 class backup_database extends base {
   function backup_database() {
-    global $CONF, $DB;
+    global $CONF, $DB, $FORM, $LNG, $TMPL;
 
-    $tables_to_backup = array(
-      "{$CONF['sql_prefix']}_settings" => true,
-      "{$CONF['sql_prefix']}_bad_words" => true,
-      "{$CONF['sql_prefix']}_ban" => true,
-      "{$CONF['sql_prefix']}_custom_pages" => true,
-      "{$CONF['sql_prefix']}_etc" => true,
-      "{$CONF['sql_prefix']}_categories" => true,
-      "{$CONF['sql_prefix']}_ip_log" => false,
-      "{$CONF['sql_prefix']}_reviews" => true,
-      "{$CONF['sql_prefix']}_sessions" => false,
-      "{$CONF['sql_prefix']}_sites" => true,
-      "{$CONF['sql_prefix']}_stats" => true,
-    );
+    if (!isset($FORM['submit'])) {
+      $TMPL['header'] = $LNG['a_backup_header'];
 
-    header('Pragma: no-cache');
-    header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
-    header('Content-Disposition: attachment; filename="' . $CONF['list_name'] . date(' Y-m-d') . '.sql"');
-    header('Content-Type: text/x-sql');
-    foreach($tables_to_backup as $table_name => $save_data) {
-      echo $DB->get_table($table_name, $save_data);
+      $TMPL['admin_content'] = <<<EndHTML
+{$LNG['a_backup_warn']}<br /><br />
+<form action="{$TMPL['list_url']}/index.php?a=admin&amp;b=backup_database" method="post">
+<input type="submit" name="submit" value="{$LNG['a_backup_header']}" />
+</form>
+EndHTML;
     }
+    else {
+      $tables_to_backup = array(
+        "{$CONF['sql_prefix']}_settings" => true,
+        "{$CONF['sql_prefix']}_bad_words" => true,
+        "{$CONF['sql_prefix']}_ban" => true,
+        "{$CONF['sql_prefix']}_custom_pages" => true,
+        "{$CONF['sql_prefix']}_etc" => true,
+        "{$CONF['sql_prefix']}_categories" => true,
+        "{$CONF['sql_prefix']}_ip_log" => false,
+        "{$CONF['sql_prefix']}_reviews" => true,
+        "{$CONF['sql_prefix']}_sessions" => false,
+        "{$CONF['sql_prefix']}_sites" => true,
+        "{$CONF['sql_prefix']}_stats" => true,
+      );
 
-    exit;
+      header('Pragma: no-cache');
+      header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+      header('Content-Disposition: attachment; filename="' . $CONF['list_name'] . date(' Y-m-d') . '.sql"');
+      header('Content-Type: text/x-sql');
+      foreach($tables_to_backup as $table_name => $save_data) {
+        echo $DB->get_table($table_name, $save_data);
+      }
+
+      exit;
+    }
   }
 }
 ?>
